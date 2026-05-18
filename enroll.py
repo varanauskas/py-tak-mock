7d6de03d9cd3fcb76c5a7affa9b9c426e0c29000
Tadas Varanauskas <tadas@varanauskas.lt>
2026-05-18 09:50:16 +0300

    Update flow

--- enroll.py
+++ enroll.py
@@ -1,18 +1,27 @@
 from cryptography.hazmat.primitives import serialization, hashes
 
-def create_and_enroll_client_certificate(domain, enrollment_port, username, password, ssl_verify=True):
-    import ssl
+def auth_request(url, username, password, method="GET", data=None, content_type=None, ssl_context=None):
     from urllib import request
     from urllib.request import Request, urlopen
+
+    req = Request(url, data=data, method=method)
+    req.add_header("Authorization", f"Basic {request.base64.b64encode(f'{username}:{password}'.encode()).decode()}")
+    if content_type:
+        req.add_header("Content-Type", content_type)
+    return urlopen(req, context=ssl_context)
+
+def create_and_enroll_client_certificate(domain, enrollment_port, username, password, ssl_context=None):
     from xml.etree import ElementTree as ET
     from cryptography import x509
     from cryptography.hazmat.primitives.asymmetric import rsa
     import json
 
-    ssl_context = None if ssl_verify else ssl._create_unverified_context()
-    config_req = Request(f"https://{domain}:{enrollment_port}/Marti/api/tls/config")
-    config_req.add_header("Authorization", f"Basic {request.base64.b64encode(f'{username}:{password}'.encode()).decode()}")
-    tls_config = ET.fromstring(urlopen(config_req, context=ssl_context).read())
+    tls_config = ET.fromstring(auth_request(
+        f"https://{domain}:{enrollment_port}/Marti/api/tls/config",
+        username,
+        password,
+        ssl_context=ssl_context
+    ).read())
 
     rfc_4514_string = f"CN={username}"
     for elem in tls_config.iter():
@@ -28,10 +37,15 @@ def create_and_enroll_client_certificate(domain, enrollment_port, username, pass
 
     csr = x509.CertificateSigningRequestBuilder().subject_name(subject).sign(private_key, hashes.SHA256()).public_bytes(serialization.Encoding.PEM)
 
-    req = Request(f"https://{domain}:{enrollment_port}/Marti/api/tls/signClient/v2", data=csr, method="POST")
-    req.add_header("Content-Type", "application/pkcs10")
-    req.add_header("Authorization", f"Basic {request.base64.b64encode(f'{username}:{password}'.encode()).decode()}")
-    with urlopen(req, context=ssl_context) as response:
+    with auth_request(
+        f"https://{domain}:{enrollment_port}/Marti/api/tls/signClient/v2",
+        username,
+        password,
+        method="POST",
+        data=csr,
+        content_type="application/pkcs10",
+        ssl_context=ssl_context
+    ) as response:
         data = json.load(response)
         signed_cert_pem = f"-----BEGIN CERTIFICATE-----\n{data.get('signedCert').replace('\\n', '\n')}\n-----END CERTIFICATE-----"
         ca_pems = []
@@ -50,11 +64,19 @@ def create_and_enroll_client_certificate(domain, enrollment_port, username, pass
         return private_key, certificate, ca_certificates
     
 if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(
+        prog="enroll.py",
+        description="Enroll client certificate and save it to client_cert.pem and client_key.pem",
+    )
+    parser.add_argument("host", nargs=1, help="Domain or IP address of the server to enroll with")
+    parser.add_argument("-port", "-p", nargs="?", type=int, default=8446, help="Enrollment port of the server (default: 8446)")
+    parser.add_argument("--unverified-ssl", action="store_true", help="Don't verify SSL certificate of the server (not recommended)")
     enrollment_port = 8446
     domain = input("Domain: ")
     username = input("Username: ")
     password = input("Password: ")
-    private_key, certificate, ca_certificates = create_and_enroll_client_certificate(domain, enrollment_port, username, password, False)
+    private_key, certificate, ca_certificates = create_and_enroll_client_certificate(domain, enrollment_port, username, password)
     with open("client_cert.pem", "wb") as f:
         f.write(certificate.public_bytes(serialization.Encoding.PEM) + b"".join(ca_cert.public_bytes(serialization.Encoding.PEM) for ca_cert in ca_certificates))
     with open("client_key.pem", "wb") as f:
